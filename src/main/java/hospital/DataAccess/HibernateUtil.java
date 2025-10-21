1c4634528fbbda999b77285aec2c7d1bf8cfdc2b
Sebastian B <141260528+Killazz17@users.noreply.github.com>
2025-10-20 20:11:06 -0600

    Config structure

---
+++ src/main/java/hospital/DataAccess/HibernateUtil.java
@@ -0,0 +1,35 @@
+package hospital.DataAccess;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties"));
+
+            sessionFactory = new Configuration()
+                    .setProperties(properties)
+
+                    // Agregar aqui otras entidades.
+                    .buildSessionFactory();
+
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError("Could not load hibernate.properties: " + e.getMessage());
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/main/java/hospital/Server/SocketServer.java
@@ -0,0 +1,4 @@
+package hospital.Server;
+
+public class SocketServer {
+}
